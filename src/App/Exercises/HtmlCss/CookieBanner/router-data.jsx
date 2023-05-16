0aedb9960dde06a8f5b7c87d775e8f7482c26578
Karina Złotogórska-Krzemińska <karina.zlotogorska@pomeranianstartit.pl>
2023-05-16 16:54:36 +0200

    Create cookies banner

---
+++ src/App/Exercises/HtmlCss/CookieBanner/router-data.jsx
@@ -0,0 +1,24 @@
+import { CookieBanner } from './CookieBanner';
+
+export const blockRouterMetaData = {
+  path: 'cookie-banner',
+  date: '15-05-2023',
+  linkLabel: 'CookieBanner',
+  blockNo: 19,
+  element: <CookieBanner />,
+  tags: [
+    'z-index',
+
+    'positioning',
+
+    'css',
+
+    'float',
+
+    'absolute',
+
+    'fixed',
+
+    'relative',
+  ],
+};
